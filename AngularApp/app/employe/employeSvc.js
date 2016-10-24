7faea8e4924bcb1889935572ace1a41b5e59c206
sreejan2324 <sreejan2324.gutta@gmail.com>
2016-10-24 14:03:52 +0530

    angularja employe asssignment

--- AngularApp/app/employe/employeSvc.js
+++ AngularApp/app/employe/employeSvc.js
@@ -1,9 +1,9 @@
 (function (){
     
  function employeSvc(){
-    this.getEmployes=function()
+    this.getEmployes=function($scope)
     {
-        var employes=[{
+       $scope.employes=[{
             
             employeId:"12BJ1A501",
             employeName:"thri",
@@ -28,9 +28,34 @@
             employeSalary:70000,
         }
                       ];
-        return employes;
+        
+         return employes;
+        
+      
+        
+        $scope.addRow = function(){		
+	/*$scope.companies.push({ 'E.Id':$scope.employeId, 'E.Name': $scope.employeName, 'E.Salary':$scope.employeSalary });*/
+	$scope.employeId='';
+	$scope.employeName='';
+	$scope.employeSalary='';
+};
+$scope.removeRow = function(employeId){				
+		var index = -1;		
+		var comArr = eval( $scope.employes );
+		for( var i = 0; i < comArr.length; i++ ) {
+			if( comArr[i].employeId === employeId ) {
+				index = i;
+				break;
+			}
+		}
+		if( index === -1 ) {
+			alert( "Something gone wrong" );
+		}
+		$scope.employes.splice( index, 1 );
+};
+     };    
             
-        };
+        
     }
      
     
